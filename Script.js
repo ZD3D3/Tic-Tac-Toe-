755606b28b56e574107d4e8c16f771dfaf023947
ZD3D3 <zcd092010@gmail.com>
2024-03-13 00:14:22 -0400

    JS Stuff

--- Script.js
+++ Script.js
@@ -1,2 +1,23 @@
+// Variables for project
 let turn = 0;
 let icon = ["X","O"];
+
+
+startBtn = document.getElementById("startBtn");
+grid = document.getElementById("grid-container").addEventListener(click, gridClick);
+
+//Functions for project 
+function startGame(){
+    
+};
+
+function checkWin(playerWin,computerWin,Draw){
+
+};
+
+function gridClick(){
+        
+    }
+
+
+
